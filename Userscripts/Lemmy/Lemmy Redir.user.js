bd29352192912c5bec4c23968d6e2176519838ce
ikeman2003 <96934345+ikeman2003@users.noreply.github.com>
2023-06-16 13:48:32 -0400

    Update Lemmy Redir.user.js
    add many new redirects

--- Userscripts/Lemmy/Lemmy Redir.user.js
+++ Userscripts/Lemmy/Lemmy Redir.user.js
@@ -1,9 +1,14 @@
 // ==UserScript==
 // @name           Lemmy/KBin Redir
-// @version        0.1.1
+// @version        0.2.0
 // @author         ikeman2003
 // @description    Redirects all Lemmy/kbin links to the host of your choice mine being kbin.
 // @match          *://*.lemmy.ml/c/*   
+// @match          *://*.lemmynsfw.com/c/*
+// @match          *://*.sh.itjust.works/c/*
+// @match          *://*.beehaw.org/c/*
+// @match          *://*.lemmy.world/c/*
+// @match          *://*.midwest.social/c/*
 // @downloadURL    https://github.com/ikeman2003/UserscriptRepo/raw/main/Userscripts/Lemmy/Lemmy%20Redir.user.js
 // @updateURL      https://github.com/ikeman2003/UserscriptRepo/raw/main/Userscripts/Lemmy/Lemmy%20Redir.user.js
 // @run-at         document-start
@@ -21,6 +26,20 @@ const hostURL = 'https://kbin.social/m/';
 // Check if the current URL is leading to lemmy.ml if so, redirect to the host URL
 if (currentURL.includes("lemmy.ml/c/")) {
     const newURL = currentURL.replace(/^https?:\/\/(www\.)?lemmy.ml\/c\//, hostURL);
-    window.location.replace(newURL+"@lemmy.ml");
+    window.location.replace(newURL + "@lemmy.ml");
+} else if (currentURL.includes("https://lemmynsfw.com/c/")) {
+    const newURL = currentURL.replace(/^https?:\/\/(www\.)?lemmynsfw\.com\/c\//, hostURL);
+    window.location.replace(newURL + "@lemmynsfw.com");
+} else if (currentURL.includes("https://sh.itjust.works/c/")) {
+    const newURL = currentURL.replace(/^https?:\/\/(www\.)?sh\.itjust\.works\/c\//, hostURL);
+    window.location.replace(newURL + "@sh.itjust.works");
+} else if (currentURL.includes("https://beehaw.org/c/")) {
+    const newURL = currentURL.replace(/^https?:\/\/(www\.)?beehaw\.org\/c\//, hostURL);
+    window.location.replace(newURL + "@beehaw.org");
+} else if (currentURL.includes("https://lemmy.world/c/")) {
+    const newURL = currentURL.replace(/^https?:\/\/(www\.)?lemmy\.world\/c\//, hostURL);
+    window.location.replace(newURL + "@lemmy.world");
+} else if (currentURL.includes("https://midwest.social/c/")) {
+    const newURL = currentURL.replace(/^https?:\/\/(www\.)?midwest\.social\/c\//, hostURL);
+    window.location.replace(newURL + "@midwest.social");
 }
-
