461482a765f01bfa6ea1dbec63a2d42ae275b7cc
ikeman2003 <96934345+ikeman2003@users.noreply.github.com>
2025-05-27 21:35:01 -0400

    Add Discord folder and no reply mentions scrtipt

---
+++ Userscripts/Discord/No Reply Mentions.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         Discord - Do Not Mention on Replies By Default
+// @version      1.0
+// @description  Sets Mention on Reply to Off by default, and adds Alt+m as a keyboard shortcut to toggle
+// @author       ikeman2003 (fork of Suyooo's version)
+// @license      Apache-2.0
+// @match        https://discordapp.com/*
+// @match        https://discord.com/*
+// @grant        none
+// @require      http://code.jquery.com/jquery-latest.js
+// @downloadURL  https://raw.githubusercontent.com/ikeman2003/UserscriptRepo/main/Userscripts/Discord/No%20Reply%20Mentions.user.js
+// @updateURL    https://raw.githubusercontent.com/ikeman2003/UserscriptRepo/main/Userscripts/Discord/No%20Reply%20Mentions.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let mutationObserver = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            mutation.addedNodes.forEach( function(currentValue, currentIndex, listObj) {
+                if (currentValue.nodeType == Node.ELEMENT_NODE) {
+                    $("div[class*='mentionButton_'][style*='color: var(--text-link);']", currentValue).click();
+                }
+            });
+        });
+    });
+    mutationObserver.observe(document.documentElement, {
+        childList: true,
+        subtree: true
+    });
+
+    function onKeyDown(e) {
+        if (e.key == "m" && e.altKey) {
+            $("div[class*='mentionButton_']").click();
+        }
+    }
+    window.addEventListener("keydown", onKeyDown, true);
+})();
