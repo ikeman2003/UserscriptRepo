7be17577edbb03050ef93195b9c923e693b8f31f
ikeman2003 <96934345+ikeman2003@users.noreply.github.com>
2026-03-02 15:30:09 -0500

    Update No Reply Mentions.user.js
    Fix the keyboard shortcut classing

--- Userscripts/Discord/No Reply Mentions.user.js
+++ Userscripts/Discord/No Reply Mentions.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Discord - Do Not Mention on Replies By Default
-// @version      1.2.2
+// @version      1.2.3
 // @description  Sets Mention on Reply to Off by default, and adds Alt+p as a keyboard shortcut to toggle
 // @author       ikeman2003 (fork of Suyooo's version)
 // @license      Apache-2.0
@@ -31,7 +31,7 @@
 
     function onKeyDown(e) {
         if (e.key == "p" && e.altKey) {
-            $("div[class*='_841c8f861f7b2704-mentionButton']").click();
+            $("div[class*='mentionButton__841c8']").click();
         }
     }
     window.addEventListener("keydown", onKeyDown, true);
